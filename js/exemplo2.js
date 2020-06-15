6596da454e1ecdad2e5ff30296e170808aa734b9
Rafael <rafaelhr.ribeiro@gmail.com>
2020-06-15 14:28:03 -0300

    Exemplo 2 - setInteval()

---
+++ js/exemplo2.js
@@ -0,0 +1,10 @@
+
+var mostrarMensagem = function()   {
+    // alert("Mensagem disparada!");
+    console.log("Mensagem disparada!");
+}
+
+
+// Chamar função mostrarMensagem() a cada 2 segundos
+// Tempo: é expressado em ms, portando 2s = 2000ms
+setInterval(mostrarMensagem, 2000);
